1c9c8596b26d135d4dc195c1e53c5e7d52a04c9d
Antonio Ladeia <contato@antonioladeia.com>
2018-07-14 07:28:55 -0300

    formatting code for python codestyle

--- py-bs/acessandoPaginas.py
+++ py-bs/acessandoPaginas.py
@@ -5,109 +5,126 @@ import requests
 from bs4 import BeautifulSoup
 
 paginas = []
-dadosRequisicao = False
+dados_requisicao = False
 
-def indicePaginador( pagina ):
-	indice = 0
 
-	if pagina < 12:
-		indice = pagina - 1
+def indice_paginador(pagina):
+    indice = 0
 
-	else:
-		indice = pagina % 10
+    if pagina < 12:
+        indice = pagina - 1
 
-		if indice < 2:
-			indice += 10
-	
-	strIndice = str( indice )
+    else:
+        indice = pagina % 10
 
-	if len( strIndice ) == 1:
-		strIndice = '0'+strIndice
+        if indice < 2:
+            indice += 10
 
-	return strIndice
+    str_indice = str(indice)
 
-def pagina( params ):
-	r = False
-	registros = []
+    if len(str_indice) == 1:
+        str_indice = '0' + str_indice
 
-	if params:
-		pagina = params['pagina']
-		indice = indicePaginador( pagina )
-		
-		dados = {
-			'__VIEWSTATE': params['viewState'],
-			'__VIEWSTATEGENERATOR': '48B4125A',
-			'__EVENTVALIDATION': params['eventValidation'],
-			'__EVENTTARGET': 'ctl00$ContentPlaceHolder1$dpNoticia$ctl01$ctl'+indice,
-			'ctl00$ContentPlaceHolder1$ToolkitScriptManager1': 'ctl00$ContentPlaceHolder1$UpdatePanel1|ctl00$ContentPlaceHolder1$dpNoticia$ctl01$ctl'+indice
-		}
+    return str_indice
 
-		r = requests.post( 'http://www.cms.ba.gov.br/despesa.aspx', data=dados )
 
-	else:
-		r = requests.get( 'http://www.cms.ba.gov.br/despesa.aspx' )
+def pagina(params):
+    r = False
+    registros = []
 
-	parsed_html = BeautifulSoup( r.text, 'html.parser' )
+    if params:
+        pagina = params['pagina']
+        indice = indice_paginador(pagina)
 
-	paginador = parsed_html.body.find('span', attrs={'id':'ContentPlaceHolder1_dpNoticia'})
-	paginaAtual = int( paginador.find('span').text )
-	linksPaginas = paginador.find_all('a')
+        dados = {
+            '__VIEWSTATE': params['viewState'],
+            '__VIEWSTATEGENERATOR': '48B4125A',
+            '__EVENTVALIDATION': params['eventValidation'],
+            '__EVENTTARGET':
+                'ctl00$ContentPlaceHolder1$dpNoticia$ctl01$ctl' + indice,
+            'ctl00$ContentPlaceHolder1$ToolkitScriptManager1': 'ctl00$ContentPlaceHolder1$UpdatePanel1|ctl00$ContentPlaceHolder1$dpNoticia$ctl01$ctl' + indice
+        }
 
-	temProxima = False
-	strListaPaginas = ''
-	contadorInteracaoPaginador = 0
+        r = requests.post('http://www.cms.ba.gov.br/despesa.aspx', data=dados)
 
-	for link in linksPaginas:
-		contadorInteracaoPaginador += 1
-		strListaPaginas += link.text + ' '
+    else:
+        r = requests.get('http://www.cms.ba.gov.br/despesa.aspx')
 
-		if not temProxima:
-			if link.text.isnumeric() and paginaAtual and int( link.text ) > paginaAtual:
-				temProxima = True
+    parsed_html = BeautifulSoup(r.text, 'html.parser')
 
-			elif contadorInteracaoPaginador > 2 and link.text == '...':
-				temProxima = True
+    paginador = parsed_html.body.find(
+        'span',
+        attrs={'id': 'ContentPlaceHolder1_dpNoticia'}
+    )
+    pagina_atual = int(paginador.find('span').text)
+    links_paginas = paginador.find_all('a')
 
-	novoViewState = parsed_html.body.find('input', attrs={'id':'__VIEWSTATE'})["value"]
-	novoEventValidation = parsed_html.body.find('input', attrs={'id':'__EVENTVALIDATION'})["value"]
+    tem_proxima = False
+    strListaPaginas = ''
+    contador_interacao_paginador = 0
 
-	areaDespesas = parsed_html.body.find('div', attrs={'id':'ContentPlaceHolder1_UpdatePanel1'})
-	divsInternas = areaDespesas.find_all( 'div' )
-	i = 0
+    for link in links_paginas:
+        contador_interacao_paginador += 1
+        strListaPaginas += link.text + ' '
 
-	for a in divsInternas:
-		i += 1
+        if not tem_proxima:
+            if link.text.isnumeric() and \
+                    pagina_atual and int(link.text) > pagina_atual:
+                tem_proxima = True
 
-		if i > len( divsInternas ) - 5 and i < len( divsInternas ):
-			registros.append( a.text )
+            elif contador_interacao_paginador > 2 and link.text == '...':
+                tem_proxima = True
 
-	return ({
-		'temMais': temProxima,
-		'registros': registros,
-		'paginas': strListaPaginas,
-		'viewState': novoViewState,
-		'paginaAtual': paginaAtual,
-		'eventValidation': novoEventValidation
-	})
+    novo_view_state = parsed_html.body.find(
+        'input',
+        attrs={'id': '__VIEWSTATE'}
+    )["value"]
+    novo_event_validation = parsed_html.body.find(
+        'input',
+        attrs={'id': '__EVENTVALIDATION'}
+    )["value"]
+
+    area_despesas = parsed_html.body.find(
+        'div',
+        attrs={'id': 'ContentPlaceHolder1_UpdatePanel1'}
+    )
+    divs_internas = area_despesas.find_all('div')
+    i = 0
+
+    for a in divs_internas:
+        i += 1
+
+        if len(divs_internas) - 5 < i < len(divs_internas):
+            registros.append(a.text)
+
+    return ({
+        'temMais': tem_proxima,
+        'registros': registros,
+        'paginas': strListaPaginas,
+        'viewState': novo_view_state,
+        'paginaAtual': pagina_atual,
+        'eventValidation': novo_event_validation
+    })
+
 
 while True:
-	retorno = pagina( dadosRequisicao )
-	paginas.append({
-		'numero': retorno['paginaAtual'],
-		'registros': retorno['registros']
-	})
+    retorno = pagina(dados_requisicao)
+    paginas.append({
+        'numero': retorno['paginaAtual'],
+        'registros': retorno['registros']
+    })
 
-	print( ' ** HTML ' + str( retorno['paginaAtual'] ) )
+    print(' ** HTML ' + str(retorno['paginaAtual']))
 
-	if not retorno['temMais']:
-		break
+    if not retorno['temMais']:
+        break
 
-	dadosRequisicao = {
-		'pagina': retorno['paginaAtual']+1,
-		'viewState': retorno['viewState'],
-		'eventValidation': retorno['eventValidation']
-	}
+    dados_requisicao = {
+        'pagina': retorno['paginaAtual'] + 1,
+        'viewState': retorno['viewState'],
+        'eventValidation': retorno['eventValidation']
+    }
 
-	time.sleep( 0.2 )
+    time.sleep(0.2)
 
-print( paginas )
+print(paginas)
