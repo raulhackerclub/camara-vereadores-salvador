6fad3882be15e4799edd6acc543f76023c8e4a89
Antonio Ladeia <contato@antonioladeia.com>
2018-07-14 11:25:57 -0300

    first code for change pager (no result)

--- py-scrapy/vereadores/vereadores/spiders/cms.py
+++ py-scrapy/vereadores/vereadores/spiders/cms.py
@@ -18,14 +18,47 @@ class CmsSpider(scrapy.Spider):
     def clean_line(line):
         return line[1::2]
 
-    def parse(self, response):
+    @staticmethod
+    def has_next(div_pagination, actual_page):
+        dp = div_pagination.xpath('./a/text()').extract()
+        position = dp.index(str(actual_page))
+        if dp[position+1] != '>':
+            return True
+        else:
+            return False
 
+    def parse(self, response):
+        div_pagination = response.xpath(
+            '//*[@id="ContentPlaceHolder1_dpNoticia"]'
+        )
+
+        view_state = response.xpath('//*[@id="__VIEWSTATE"]').extract_first()
+        event_validation = response.xpath('//*[@id="__EVENTVALIDATION"]').extract_first()
+        actual_page = int(div_pagination.xpath('./span/text()').extract_first()) + 1
+
+        while self.has_next(div_pagination, actual_page):
+
+            yield scrapy.Request(
+                url="http://www.cms.ba.gov.br/despesa.aspx/",
+                callback=self.parse_detail,
+                method='POST',
+                headers={
+                    'pagina': actual_page,
+                    'viewState': view_state,
+                    'eventValidation': event_validation,
+                    '__EVENTTARGET':
+                        'ctl00$ContentPlaceHolder1$dpNoticia$ctl01$ctl%s' % (actual_page + 1),
+                        'ctl00$ContentPlaceHolder1$ToolkitScriptManager1':
+                        'ctl00$ContentPlaceHolder1$UpdatePanel1|ctl00$ContentPlaceHolder1$dpNoticia$ctl01$ctl%s'
+                        % (actual_page + 1)
+                }
+            )
+
+    def parse_detail(self, response):
         divs_data = response.xpath(
             '//*[@id="ContentPlaceHolder1_UpdatePanel1"]/div'
         )
-        # div_pagination = response.xpath(
-        # '//*[@id="ContentPlaceHolder1_dpNoticia"]'
-        # )
+
         clean_divs_data = self.clean_list(divs_data)
 
         for div in clean_divs_data:
